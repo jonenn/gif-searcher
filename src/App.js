3f5763a9d985ba2c4dbc93e5b910480bb9821f0b
Juan J Malaver <juanmagri@outlook.es>
2022-05-30 19:20:08 -0500

    Created categories list and component in which you can add them

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+   const [categories, setCategories] = useState(["Cat", "Dog", "Bunny"]);
+   // const handleAdd = () => {
+   //    setCategories([...categories, "Turtle"]);
+   // };
 
-export default App;
+   return (
+      <>
+         <AddCategory setCategories={setCategories} />
+         <hr />
+         <ol>
+            {categories.map((category) => {
+               return <li key={category}>{category}</li>;
+            })}
+         </ol>
+         <button>Add</button>
+      </>
+   );
+};
+
+export { App };
---
+++ src/components/AddCategory.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const AddCategory = ({ setCategories }) => {
+   const [inputValue, setInputValue] = useState("hi world!");
+
+   const handleInput = (eve) => {
+      setInputValue(eve.target.value);
+   };
+
+   const handleSubmit = (eve) => {
+      eve.preventDefault();
+      console.log("Done!!");
+      setCategories((categories) => [...categories, inputValue]);
+   };
+
+   return (
+      <form onSubmit={handleSubmit}>
+         <h1>{inputValue}</h1>
+         <input type="text" value={inputValue} onChange={handleInput} />
+      </form>
+   );
+};
